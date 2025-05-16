a1cd15eff8a94458a4aad3135dadf498d2194a9c
橋本麟太郎 <hashimotorintarou@MacBook-Air.local>
2025-05-16 11:47:27 +0900

    07_mypage

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,23 @@
 class UsersController < ApplicationController
   skip_before_action :require_login, only: %i[new create]
+  before_action :set_user
 
   def new
     @user = User.new
+  end
+
+  def edit
+    # 編集対象のフィールドをparamsで受け取る
+    @field = params[:field]
+    render partial: "edit_field", formats: [:html]
+  end
+
+  def update
+    if @user.update(user_params)
+      redirect_to mypage_path, notice: "更新しました"
+    else
+      render :show
+    end
   end
 
   def create
@@ -19,6 +34,10 @@ class UsersController < ApplicationController
   end
 
   private
+
+  def set_user
+    @user = current_user
+  end
 
   def user_params
     params.require(:user).permit(:name, :email, :password, :password_confirmation)
