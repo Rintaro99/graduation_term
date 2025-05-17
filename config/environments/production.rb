50f35968f90b425cf4669f1ccb1d6316bf0670c4
橋本麟太郎 <hashimotorintarou@MacBook-Air.local>
2025-05-17 13:33:54 +0900

    Heroku SendGrid mail settings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -103,10 +103,11 @@ Rails.application.configure do
   # Skip DNS rebinding protection for the default health check endpoint.
   # config.host_authorization = { exclude: ->(request) { request.path == "/up" } }
 
-  config.action_mailer.default_url_options = { host: 'https://cryptic-coast-70717-f9eb22b0cbd6.herokuapp.com/' }
-
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.default_url_options = { host: 'https://cryptic-coast-70717-f9eb22b0cbd6.herokuapp.com' }
+  config.action_mailer.default_url_options = {
+  host: 'cryptic-coast-70717-f9eb22b0cbd6.herokuapp.com',
+  protocol: 'https'  # ← こちらでhttpsを指定！
+  }
 
   config.action_mailer.smtp_settings = {
     user_name: 'apikey', # ←文字列で固定（間違えないで！）
