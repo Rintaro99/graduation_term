92ecaf8a3b163f3fcb7f88bb5d77454f5134c5b0
橋本麟太郎 <hashimotorintarou@MacBook-Air.local>
2025-05-17 13:05:09 +0900

    Set up SendGrid via API key

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -102,4 +102,19 @@ Rails.application.configure do
   # ]
   # Skip DNS rebinding protection for the default health check endpoint.
   # config.host_authorization = { exclude: ->(request) { request.path == "/up" } }
+
+  config.action_mailer.default_url_options = { host: 'https://cryptic-coast-70717-f9eb22b0cbd6.herokuapp.com/' }
+
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.default_url_options = { host: 'https://cryptic-coast-70717-f9eb22b0cbd6.herokuapp.com' }
+
+  config.action_mailer.smtp_settings = {
+    user_name: 'apikey', # ←文字列で固定（間違えないで！）
+    password: ENV['SENDGRID_API_KEY'],
+    domain: 'heroku.com',
+    address: 'smtp.sendgrid.net',
+    port: 587,
+    authentication: :plain,
+    enable_starttls_auto: true
+  }
 end
