85d1ceb3d21a3cfc908da92f0697c748d0cbead3
橋本麟太郎 <rin.910491@gmail.com>
2025-06-21 12:20:56 +0900

    Fix UJS import for production

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,5 +1,7 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
-import * as Rails from "@rails/ujs"
+// import * as Rails from "@rails/ujs"
+import Rails from "@rails/ujs"
+window.Rails = Rails
 
 Rails.start()
