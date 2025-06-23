e2bb1d57d37b1316ea2c8145570c340ab9d8fe82
橋本麟太郎 <rin.910491@gmail.com>
2025-06-24 08:28:34 +0900

    投稿削除機能js

--- config/importmap.rb
+++ config/importmap.rb
@@ -1,5 +1,5 @@
 # Pin npm packages by running ./bin/importmap
 
 pin "application"
-pin "@rails/ujs", to: "rails-ujs.js", preload: true
+# pin "@rails/ujs", to: "rails-ujs.js", preload: true
 pin "@hotwired/turbo-rails", to: "turbo.min.js"
