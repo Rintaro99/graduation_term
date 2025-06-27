867c2d2dac66656d7ae3b4bfa3d9edbfd7f9c1e8
橋本麟太郎 <rin.910491@gmail.com>
2025-06-27 17:04:09 +0900

    環境構築

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,6 +1,25 @@
-// Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
-import "@hotwired/turbo-rails"
-// import * as Rails from "@rails/ujs"
+console.log("✅ application.js 読み込まれました！")
 
-window.Rails = Rails
-// Rails.start()
+
+// Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
+// import "@hotwired/turbo-rails"
+import Rails from "@rails/ujs"
+Rails.start()
+
+import * as Turbo from "@hotwired/turbo"
+window.Turbo = Turbo
+Turbo.start()
+
+// import "@hotwired/turbo-rails"
+
+// window.Rails = Rails
+
+import React from "react"
+import { createRoot } from "react-dom/client"
+import HelloReact from "./components/HelloReact"
+
+const container = document.getElementById("react-root")
+if (container) {
+  const root = createRoot(container)
+  root.render(<HelloReact />)
+}
