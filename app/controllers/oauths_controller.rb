773e861863d4c59d85f0de36ae90e9cd9f21210e
橋本麟太郎 <rin.910491@gmail.com>
2025-07-08 21:44:51 +0900

    heroku修正11

--- app/controllers/oauths_controller.rb
+++ app/controllers/oauths_controller.rb
@@ -38,6 +38,7 @@ class OauthsController < ApplicationController
 
     if @user = login_from(provider)
       Rails.logger.debug "[DEBUG] login_from succeeded: #{@user.inspect}"
+      Rails.logger.debug "[DEBUG] session[:user_id] = #{session[:user_id]}"
       auto_login(@user)
       redirect_to userpage_path, notice: "#{provider.titleize}でログインしました"
     else
