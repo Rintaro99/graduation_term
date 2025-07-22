788244d3db5efbd35be958a49ad111db68d77f24
橋本麟太郎 <rin.910491@gmail.com>
2025-07-22 16:35:04 +0900

    fix: syntax errorの対応

--- app/controllers/oauths_controller.rb
+++ app/controllers/oauths_controller.rb
@@ -18,46 +18,31 @@ class OauthsController < ApplicationController
 
   def callback
     provider = params[:provider]
-    Rails.logger.debug "[DEBUG] === callback called for #{provider} ==="
 
-    # begin
-    #   access_token = get_access_token(provider)
-    #   Rails.logger.debug "[DEBUG] access_token.uid = #{access_token.uid}"
-    #   Rails.logger.debug "[DEBUG] access_token.info.email = #{access_token.info.email}"
-    # rescue => e
-    #   Rails.logger.error "[ERROR] get_access_token failed: #{e.class} - #{e.message}"
-    #   redirect_to root_path, alert: "#{provider.titleize}の認証に失敗しました"
-    #   return
-    # end
-    # access_token = get_access_token(provider)
-
-    # Rails.logger.debug "[DEBUG] access_token = #{access_token.inspect}"
-    # Rails.logger.debug "[DEBUG] access_token.uid = #{access_token&.uid}"
-    # Rails.logger.debug "[DEBUG] access_token.info.email = #{access_token&.info&.email}"
-    # Rails.logger.debug "[DEBUG] access_token.raw_info = #{access_token&.raw_info&.inspect}"
-
-    if @user = login_from(provider)
-      auto_login(@user)
-      redirect_to userpage_path, notice: "#{provider.titleize}でログインしました"
-    else
-      access_token = get_access_token(provider)
-      email = access_token.info.email
-      uid   = access_token.uid
-      
-      @user = User.find_by(email: email)
-
-      if @user
-        @user.authentications.find_or_creat_by(provider: provider, uid: uid)
-      else
-        @user = create_from(provider)
-      end
-        # reset_session
+    begin
+      if @user = login_from(provider)
         auto_login(@user)
         redirect_to userpage_path, notice: "#{provider.titleize}でログインしました"
-    end
-    rescue StandardError => e
-      Rails.logger.error "[ERROR] create_from failed: #{e.class} - #{e.message}"
+      else
+        access_token = get_access_token(provider)
+        email = access_token.info.email
+        uid   = access_token.uid
+
+        @user = User.find_by(email: email)
+
+        if @user
+          @user.authentications.find_or_create_by(provider: provider, uid: uid)
+        else
+          @user = create_from(provider)
+        end
+
+        auto_login(@user)
+        redirect_to userpage_path, notice: "#{provider.titleize}でログインしました"
+      end
+    rescue => e
+      Rails.logger.error "[ERROR] OAuth login failed: #{e.class} - #{e.message}"
       redirect_to root_path, alert: "#{provider.titleize}でのログインに失敗しました"
     end
   end
+
 end
