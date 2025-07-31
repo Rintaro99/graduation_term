bac55c98045e15656dcbab79e5fde256740b75c1
橋本麟太郎 <rin.910491@gmail.com>
2025-07-31 13:48:00 +0900

    fix: email一意制約によるエラーの解消

--- app/controllers/oauths_controller.rb
+++ app/controllers/oauths_controller.rb
@@ -19,14 +19,17 @@ class OauthsController < ApplicationController
     provider = auth_params[:provider]
     # 既存のユーザーをプロバイダ情報を元に検索し、存在すればログイン
     if (@user = login_from(provider))
-      redirect_to root_path, notice:"#{provider.titleize}アカウントでログインしました"
+      auto_login(@user)
+      redirect_to userpage_path, notice: "#{provider.titleize}でログインしました"
     else
-      begin
-        # ユーザーが存在しない場合はプロバイダ情報を元に新規ユーザーを作成し、ログイン
-        signup_and_login(provider)
-        redirect_to root_path, notice:"#{provider.titleize}アカウントでログインしました"
-      rescue
-        redirect_to root_path, alert:"#{provider.titleize}アカウントでのログインに失敗しました"
+      # すでにメールアドレスでユーザーが存在するか確認
+      user = User.find_by(email: access_token.info.email)
+      if user
+        user.authentications.create(provider: provider, uid: access_token.uid)
+        auto_login(user)
+      else
+        @user = create_from(provider)
+        auto_login(@user)
       end
     end
   end
