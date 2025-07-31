01434b5e2fee1cf4dd3b9df63270df6fc71b60ec
橋本麟太郎 <rin.910491@gmail.com>
2025-07-31 13:22:46 +0900

    fix: email一意制約によるエラーの解消

--- app/controllers/oauths_controller.rb
+++ app/controllers/oauths_controller.rb
@@ -16,37 +16,62 @@ class OauthsController < ApplicationController
   end
 
   def callback
-    provider = params[:provider]
-    Rails.logger.debug "[DEBUG] === CALLBACK for #{provider} ==="
-
-    if @user = login_from(provider)
-      auto_login(@user)
-      Rails.logger.debug "[DEBUG] login_from success: #{@user.inspect}"
+    provider = auth_params[:provider]
+    # 既存のユーザーをプロバイダ情報を元に検索し、存在すればログイン
+    if (@user = login_from(provider))
+      redirect_to root_path, notice:"#{provider.titleize}アカウントでログインしました"
     else
       begin
-        access_token = get_access_token(provider)
-        email = access_token[:info][:email]
-        uid   = access_token[:uid]
-
-        @user = User.find_by(email: email)
-
-        if @user
-          @user.authentications.create(provider: provider, uid: uid)
-        else
-          @user = create_from(provider)
-        end
-
-        reset_session
-        auto_login(@user)
-        redirect_to userpage_path, notice: "#{provider.titleize}でログインしました"
-
-      rescue ActiveRecord::RecordNotUnique
-        flash[:alert] = "すでに同じメールアドレスが登録されています。別のログイン方法を試してください。"
-        redirect_to root_path
+        # ユーザーが存在しない場合はプロバイダ情報を元に新規ユーザーを作成し、ログイン
+        signup_and_login(provider)
+        redirect_to root_path, notice:"#{provider.titleize}アカウントでログインしました"
+      rescue
+        redirect_to root_path, alert:"#{provider.titleize}アカウントでのログインに失敗しました"
       end
     end
+  end
 
-    Rails.logger.debug "[DEBUG] session[:user_id] = #{session[:user_id]}"
+  private
+
+  def auth_params
+    params.permit(:code, :provider)
+  end
+
+  def signup_and_login(provider)
+    @user = create_from(provider)
+    reset_session
+    auto_login(@user)
+    # provider = params[:provider]
+    # Rails.logger.debug "[DEBUG] === CALLBACK for #{provider} ==="
+
+    # if (@user = login_from(provider))
+    #   auto_login(@user)
+    #   Rails.logger.debug "[DEBUG] login_from success: #{@user.inspect}"
+    # else
+    #   begin
+    #     access_token = get_access_token(provider)
+    #     email = access_token[:info][:email]
+    #     uid   = access_token[:uid]
+
+    #     @user = User.find_by(email: email)
+
+    #     if @user
+    #       @user.authentications.create(provider: provider, uid: uid)
+    #     else
+    #       @user = create_from(provider)
+    #     end
+
+    #     reset_session
+    #     auto_login(@user)
+    #     redirect_to userpage_path, notice: "#{provider.titleize}でログインしました"
+
+    #   rescue ActiveRecord::RecordNotUnique
+    #     flash[:alert] = "すでに同じメールアドレスが登録されています。別のログイン方法を試してください。"
+    #     redirect_to root_path
+    #   end
+    # end
+
+    # Rails.logger.debug "[DEBUG] session[:user_id] = #{session[:user_id]}"
     # redirect_to userpage_path, notice: "#{provider.titleize}でログインしました"
   # rescue => e
   #   Rails.logger.error "[ERROR] OAuth login failed: #{e.class} - #{e.message}"
@@ -79,5 +104,5 @@ class OauthsController < ApplicationController
   #   @user = create_from(provider)
   #   reset_session
   #   auto_login(@user)
-  # end
+  end
 end
