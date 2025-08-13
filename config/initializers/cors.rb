73fbed04d750ec8e3be098824b2aa50d07ebbf91
橋本麟太郎 <rin.910491@gmail.com>
2025-08-13 09:47:22 +0900

    reactフロントエンドをesbuild構築から、railsバックエンドと分離構築に移行：vercelにデプロイする下準備

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -2,8 +2,10 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
     origins "http://localhost:5173"
 
-    resource "*",
+    resource "/api/*",
       headers: :any,
-      methods: [:get, :post, :put, :patch, :delete, :options, :head]
+      expose: %w[Authorization],
+      methods: [:get, :post, :put, :patch, :delete, :options, :head],
+      max_age: 600
   end
 end
