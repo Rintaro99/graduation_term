0640d7fa0f7e3ad69576b8014657c21f514c9a7d
橋本麟太郎 <rin.910491@gmail.com>
2025-09-19 13:30:32 +0900

    Remove skip_before_action :require_login from OauthsController

--- app/controllers/oauths_controller.rb
+++ app/controllers/oauths_controller.rb
@@ -1,5 +1,5 @@
 class OauthsController < ApplicationController
-  skip_before_action :require_login
+  # skip_before_action :require_login
   include Sorcery::Controller::Submodules::External
 
   def oauth
