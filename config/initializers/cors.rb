e44c04387116203dff1cfe7aa16c1fd498a5410d
橋本麟太郎 <rin.910491@gmail.com>
2025-09-19 16:21:04 +0900

    Vercel のプレビュー環境をcorsに設定2

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,9 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "http://localhost:5173",  "https://how-to-write-rin.vercel.app", "https://howtowrite-9kwtq4kg5-rins-projects-f9cab347.vercel.app"
+    origins "http://localhost:5173",
+            "https://how-to-write-rin.vercel.app",
+            "https://howtowriterin.vercel.app",
+            "https://howtowriterin-git-develop-rins-projects-f9cab347.vercel.app"
 
     resource "/api/*",
       headers: :any,
