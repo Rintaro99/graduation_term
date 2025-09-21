0769827ea6484335414716a21ab07ac77a82e083
橋本麟太郎 <rin.910491@gmail.com>
2025-09-21 20:55:50 +0900

    fix: use VITE_API_BASE_URL instead of localhost

--- frontend/src/components/TestApi.tsx
+++ frontend/src/components/TestApi.tsx
@@ -4,7 +4,8 @@ export default function TestApi() {
   const [data, setData] = useState<string>('')
 
   useEffect(() => {
-    fetch('http://localhost:3000/api/test')
+    const baseURL = import.meta.env.VITE_API_BASE_URL;
+    fetch(`${baseURL}/api/test`)
       .then((res) => res.json())
       .then((data) => setData(data.message))
   }, [])
