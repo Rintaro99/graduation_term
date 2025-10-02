cb91aac77377475928161b1b6ebafcd7dec98703
橋本麟太郎 <rin.910491@gmail.com>
2025-10-02 16:57:45 +0900

    本番環境のportを465(SSL)に変更

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -114,8 +114,10 @@ Rails.application.configure do
     password: ENV['SENDGRID_API_KEY'],
     domain: 'how-to-write-rin.onrender.com',
     address: 'smtp.sendgrid.net',
-    port: 587,
+    # port: 587,
+    port: 465,
     authentication: :plain,
+    ssl: true,
     enable_starttls_auto: true
   }
 
