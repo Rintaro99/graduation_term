2ef096228c5875ff1f5d0cf1ba778e392a580f66
橋本麟太郎 <rin.910491@gmail.com>
2025-10-06 16:15:55 +0900

    Fix resend client initialization

--- app/mailers/devise_mailer.rb
+++ app/mailers/devise_mailer.rb
@@ -4,14 +4,11 @@ class DeviseMailer < Devise::Mailer
   default from: ENV["MAILER_SENDER"]
 
   def reset_password_instructions(record, token, opts = {})
-    resend = Resend::Client.new(api_key: ENV["RESEND_API_KEY"])
+    Resend.api_key = ENV["RESEND_API_KEY"]
+
     reset_link = edit_api_user_password_url(reset_password_token: token)
 
-    Rails.logger.info "=== RESEND KEY START ==="
-    Rails.logger.info ENV["RESEND_API_KEY"].inspect
-    Rails.logger.info "=== RESEND KEY END ==="
-
-    response = resend.emails.send({
+    Resend::Emails.send(
       from: ENV["MAILER_SENDER"],
       to: record.email,
       subject: "パスワードリセットのご案内",
@@ -19,8 +16,6 @@ class DeviseMailer < Devise::Mailer
         <p>以下のリンクからパスワードをリセットしてください:</p>
         <p><a href="#{reset_link}">こちらをクリック</a></p>
       HTML
-    })
-
-    Rails.logger.info "Resend response: #{response.inspect}"
+    )
   end
 end
