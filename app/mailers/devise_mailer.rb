cf3c6854038f6e1c6f687bd6e6cbf904d5ecb58d
橋本麟太郎 <rin.910491@gmail.com>
2025-10-06 16:05:49 +0900

    Add resend response log

--- app/mailers/devise_mailer.rb
+++ app/mailers/devise_mailer.rb
@@ -8,10 +8,10 @@ class DeviseMailer < Devise::Mailer
     reset_link = edit_api_user_password_url(reset_password_token: token)
 
     Rails.logger.info "=== RESEND KEY START ==="
-    Rails.logger.info ENV["RESEND_API_KEY"]
+    Rails.logger.info ENV["RESEND_API_KEY"].inspect
     Rails.logger.info "=== RESEND KEY END ==="
 
-    resend.emails.send({
+    response = resend.emails.send({
       from: ENV["MAILER_SENDER"],
       to: record.email,
       subject: "パスワードリセットのご案内",
@@ -20,5 +20,7 @@ class DeviseMailer < Devise::Mailer
         <p><a href="#{reset_link}">こちらをクリック</a></p>
       HTML
     })
+
+    Rails.logger.info "Resend response: #{response.inspect}"
   end
 end
