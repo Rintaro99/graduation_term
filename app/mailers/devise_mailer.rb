cdd9665e3f99828edf5a89c756a8f8540e0f3831
橋本麟太郎 <rin.910491@gmail.com>
2025-10-06 16:00:47 +0900

    Fix resend API argument error

--- app/mailers/devise_mailer.rb
+++ app/mailers/devise_mailer.rb
@@ -4,15 +4,14 @@ class DeviseMailer < Devise::Mailer
   default from: ENV["MAILER_SENDER"]
 
   def reset_password_instructions(record, token, opts = {})
-    Rails.logger.info "=== RESEND KEY START ==="
-    Rails.logger.info ENV["RESEND_API_KEY"].inspect
-    Rails.logger.info "=== RESEND KEY END ==="
-
-    Resend.api_key = ENV["RESEND_API_KEY"]
-
+    resend = Resend::Client.new(api_key: ENV["RESEND_API_KEY"])
     reset_link = edit_api_user_password_url(reset_password_token: token)
 
-    Resend::Emails.send(
+    Rails.logger.info "=== RESEND KEY START ==="
+    Rails.logger.info ENV["RESEND_API_KEY"]
+    Rails.logger.info "=== RESEND KEY END ==="
+
+    resend.emails.send({
       from: ENV["MAILER_SENDER"],
       to: record.email,
       subject: "パスワードリセットのご案内",
@@ -20,6 +19,6 @@ class DeviseMailer < Devise::Mailer
         <p>以下のリンクからパスワードをリセットしてください:</p>
         <p><a href="#{reset_link}">こちらをクリック</a></p>
       HTML
-    )
+    })
   end
 end
