25a84c894e164b8fd4fbbc5fd63186baeec53e53
橋本麟太郎 <rin.910491@gmail.com>
2025-10-08 21:59:07 +0900

    Add kappa-coral vercel domain to CORS origins

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,7 +1,8 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
     origins "http://localhost:5173",
-            "https://frontend-5zuvnf4fg-rins-projects-f9cab347.vercel.app"
+            "https://frontend-5zuvnf4fg-rins-projects-f9cab347.vercel.app",
+            "https://frontend-kappa-coral-85.vercel.app"
 
     resource "/api/*",
       headers: :any,
