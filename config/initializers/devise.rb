54546338787b67846c56b721059c32e9d58d5257
橋本麟太郎 <rin.910491@gmail.com>
2025-10-30 22:29:00 +0900

    Use CustomDeviseMailer for password reset with correct frontend URL

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -29,7 +29,7 @@ Devise.setup do |config|
   config.mailer_sender = "no-reply@example.com"
 
   # Configure the class responsible to send e-mails.
-  config.mailer = "DeviseMailer"
+  config.mailer = "CustomDeviseMailer"
 
   # Configure the parent class responsible to send e-mails.
   # config.parent_mailer = 'ActionMailer::Base'
