fea4a956876083558159c489b576aa97e37705e2
橋本麟太郎 <rin.910491@gmail.com>
2026-03-01 22:32:25 +0900

    refactor: migrate to API-only architecture

--- backend/db/migrate/20260301132011_drop_legacy_web_tables.rb
+++
@@ -1,8 +0,0 @@
-class DropLegacyWebTables < ActiveRecord::Migration[7.0]
-  def change
-    drop_table :authentications, if_exists: true
-    drop_table :posts, if_exists: true
-    drop_table :challenges, if_exists: true
-    drop_table :users, if_exists: true
-  end
-end
